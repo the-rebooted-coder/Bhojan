b935b98a7deaed24e1b868a929be2c82387a8ab2
Spandan Saxena <spandansaxena@gmail.com>
2021-03-14 18:06:01 +0530

    Added Sign In Functionality

--- app/src/main/java/com/aaxena/bhojan/SignUp.java
+++ app/src/main/java/com/aaxena/bhojan/SignUp.java
@@ -1,29 +1,66 @@
 package com.aaxena.bhojan;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.view.View;
 import android.view.animation.AlphaAnimation;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class SignUp extends AppCompatActivity {
     TextView fader;
     private TextView afterfade;
-    protected AlphaAnimation fadeIn = new AlphaAnimation(0.0f , 1.0f ) ;
-    protected AlphaAnimation fadeOut = new AlphaAnimation( 1.0f , 0.0f ) ;
+    protected AlphaAnimation fadeIn = new AlphaAnimation(0.0f, 1.0f);
+    protected AlphaAnimation fadeOut = new AlphaAnimation(1.0f, 0.0f);
+    private Button signInButton;
     private GoogleSignInClient mGoogleSignInClient;
     private String TAG = "Login";
-    private int RC_SIGN_IN =1;
-    String TEXT = "text";
+    private FirebaseAuth mAuth;
+    private int RC_SIGN_IN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        signInButton = findViewById(R.id.sign_in_button);
+        mAuth = FirebaseAuth.getInstance();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
 
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        signInButton.setOnClickListener(v -> {
+            Vibrator v2 = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            v2.vibrate(34);
+            vibrateDevice();
+            signIn();
+        });
+
+        //Fades the Text View Dynamically
         fader = findViewById(R.id.first_hello);
         fader.startAnimation(fadeIn);
         fader.startAnimation(fadeOut);
@@ -31,13 +68,67 @@ public class SignUp extends AppCompatActivity {
         fadeIn.setFillAfter(true);
         fadeOut.setDuration(600);
         fadeOut.setFillAfter(true);
-        fadeOut.setStartOffset(1200+fadeIn.getStartOffset());
+        fadeOut.setStartOffset(1200 + fadeIn.getStartOffset());
         afterfade = findViewById(R.id.first_hello2);
         int welcome_fade = 2000;
         new Handler().postDelayed(() -> {
             afterfade.startAnimation(fadeIn);
             afterfade.setText(R.string.welcome_to_bhojan);
         }, welcome_fade);
+    }
 
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount acc = completedTask.getResult(ApiException.class);
+            GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
+            if (account != null) {
+                String personName = account.getDisplayName();
+                Toast.makeText(SignUp.this, "Hello " + personName + " you are in!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(SignUp.this, "Welcome to Bhojan", Toast.LENGTH_LONG).show();
+                //  Intent i=new Intent(SignUp.this,Landing.class);
+                //  startActivity(i);
+                //  finish();
+            }
+            FirebaseGoogleAuth(acc);
+        } catch (ApiException e) {
+            Toast.makeText(SignUp.this, "Oops! We ran into some trouble", Toast.LENGTH_LONG).show();
+            FirebaseGoogleAuth(null);
+        }
+    }
+    private void vibrateDevice() {
+        Vibrator v3 = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v3.vibrate(VibrationEffect.createOneShot(28, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            //deprecated in API 26
+            v3.vibrate(25);
+        }
+    }
+    private void FirebaseGoogleAuth(GoogleSignInAccount acct) {
+        AuthCredential authCredential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(authCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    FirebaseUser user = mAuth.getCurrentUser();
+                } else {
+                    Toast.makeText(SignUp.this, "Failed", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
