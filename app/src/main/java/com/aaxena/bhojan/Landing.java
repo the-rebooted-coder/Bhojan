aa98bc4cc25c98c36176b038da2966e4ed8b6765
Spandan Saxena <spandansaxena@gmail.com>
2021-04-12 12:05:06 +0530

    New Bottom Nav bar

--- app/src/main/java/com/aaxena/bhojan/Landing.java
+++ app/src/main/java/com/aaxena/bhojan/Landing.java
@@ -5,36 +5,45 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import nl.joery.animatedbottombar.AnimatedBottomBar;
 
 public class Landing extends AppCompatActivity {
+    AnimatedBottomBar animatedBottomBar;
+    FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
-
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
-
+        animatedBottomBar = findViewById(R.id.bottomNavigation);
         getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,new HomeFragment()).commit();
-    }
+        animatedBottomBar.setOnTabSelectListener(new AnimatedBottomBar.OnTabSelectListener() {
+            @Override
+            public void onTabSelected(int lastIndex, @Nullable AnimatedBottomBar.Tab lastTab, int newIndex, @NotNull AnimatedBottomBar.Tab newTab) {
+                Fragment fragment = null;
+                switch (newTab.getId()){
+                    case R.id.bhojan:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.share:
+                        fragment = new ShareFragment();
+                        break;
+                    case R.id.profile:
+                        fragment = new ProfileFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,fragment).commit();
+            }
 
-    private final BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = item -> {
-        Fragment fragment = null;
-        switch (item.getItemId()){
-            case R.id.bhojan:
-                fragment = new HomeFragment();
-                break;
-            case R.id.share:
-                fragment = new ShareFragment();
-                break;
-            case R.id.profile:
-                fragment = new ProfileFragment();
-                break;
-        }
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,fragment).commit();
-        return true;
-    };
+            @Override
+            public void onTabReselected(int i, @NotNull AnimatedBottomBar.Tab tab) {
+
+            }
+        });
+    }
 }
